cf1eca2247eeb28ae70ed3098334a568de2e512d
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2025-01-13 12:45:05 +0530

    Remove redundent code

--- independent_ticketing_system/Frontend/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@radix-ui/themes";
-import { ButtonProps } from "../type";
-export default function myButton({ title, onClick }: ButtonProps) {
-  return (
-    <>
-      <Button onClick={onClick} size={"3"} style={{cursor:"pointer",minWidth:'18vh'}}>{title}</Button>
-    </>
-  );
-}
