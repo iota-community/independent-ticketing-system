d9c20ff2ed5006dd1030b4713e40e20ceae29ae7
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2025-01-13 12:36:41 +0530

    Add react router

--- independent_ticketing_system/Frontend/src/main.tsx
+++ independent_ticketing_system/Frontend/src/main.tsx
@@ -6,25 +6,73 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@iota/dapp-kit/dist/index.css";
 import "@radix-ui/themes/styles.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import { darkTheme, IotaClientProvider, WalletProvider } from "@iota/dapp-kit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Theme } from "@radix-ui/themes";
 import App from "./App.tsx";
 import { networkConfig } from "./networkConfig.ts";
+import AvailableTickets from "./components/AvailableTickets.tsx";
+import Home from "./components/Home.tsx";
+import Mint from "./components/Mint.tsx";
+import OwnedObjects from "./components/OwnedTickets.tsx";
+import ResellTickets from "./components/ResellTickets.tsx";
+import TransferTickets from "./components/TransferTickets.tsx";
+import BurnTickets from "./components/BurnTickets.tsx";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/AvailableTickets",
+        element: <AvailableTickets />,
+      },
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"/mint",
+        element: <Mint/>
+      },
+      {
+        path:"/ownedTickets",
+        element: <OwnedObjects/>
+      },
+      {
+        path:"/ownedTickets/resellTicket/:nft",
+        element:<ResellTickets/>
+      },
+      {
+        path:"/ownedTickets/transferTicket/:nft",
+        element:<TransferTickets/>
+      },
+      {
+        path: "/ownedTickets/burnTicket/:nft",
+        element: <BurnTickets/>
+      }
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Theme appearance="dark">
-      <QueryClientProvider client={queryClient}>
-        <IotaClientProvider networks={networkConfig} defaultNetwork="testnet">
-          <WalletProvider autoConnect theme={darkTheme}>
-            <App />
-          </WalletProvider>
-        </IotaClientProvider>
-      </QueryClientProvider>
-    </Theme>
+      <Theme appearance="dark">
+        <QueryClientProvider client={queryClient}>
+          <IotaClientProvider networks={networkConfig} defaultNetwork="testnet">
+            <WalletProvider autoConnect theme={darkTheme}>
+              <RouterProvider router={router} />
+            </WalletProvider>
+          </IotaClientProvider>
+        </QueryClientProvider>
+      </Theme>
   </React.StrictMode>,
 );
